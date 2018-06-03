e2284c5612c2b128103c72e5d39e1896a2340eab
vinsy <vinsy@LAPTOP-TQIIIOKJ>
2018-06-03 21:51:24 +0800

    First-time Commit

---
+++ JavaAndKotlin/src/main/java/org/lang/kotlin/App.java
@@ -0,0 +1,13 @@
+package org.lang.kotlin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
