257e5cb2ed75c2ebaa05880fbf89f49e05d2844a
XYKJAdmin <vinsyuan7788@126.com>
2018-10-09 13:07:09 +0800

    First-time Commit

---
+++ JNA/src/main/java/com/sun/jna/HelloJNA.java
@@ -0,0 +1,19 @@
+package com.sun.jna;
+
+/**
+ * 	Hello JNA (Java Native Access)
+ * 
+ * @author vinsy
+ *
+ */
+public class HelloJNA {
+	
+	/**
+	 * 	This is a main method for execution
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello JNA!");
+	}
+}
